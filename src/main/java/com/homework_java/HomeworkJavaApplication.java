9a366a6ef0d7e9b89647017ce424d169f708095d
raimondassi <raimondassiup@gmail.com>
2023-02-20 23:21:44 +0200

    first commit

---
+++ src/main/java/com/homework_java/HomeworkJavaApplication.java
@@ -0,0 +1,13 @@
+package com.homework_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkJavaApplication.class, args);
+    }
+
+}
